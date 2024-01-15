4fe9fd4085c4fdf167fd9ef05a0f0d4fd6bab652
brannon <v.lutkoff@gmail.com>
2024-01-15 23:38:31 +0300

    + custom JSON decoding & validating JSON

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,21 +6,39 @@ import (
 	"time"
 
 	"greenlight.brannon.net/internal/data"
+	"greenlight.brannon.net/internal/validator"
 )
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
 // return a plain-text placeholder response.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Title   string   `json: "title"`
-		Year    int32    `json: "year"`
-		Runtime int32    `json: "runtime"`
-		Genres  []string `json: "genres"`
+		Title   string       `json: "title"`
+		Year    int32        `json: "year"`
+		Runtime data.Runtime `json: "runtime"`
+		Genres  []string     `json: "genres"`
 	}
 
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	// Initialize a new Validator instance.
+	v := validator.New()
+
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
