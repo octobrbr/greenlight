11177e6f9576449d6c624b5cfc6eca420a05cddd
brannon <v.lutkoff@gmail.com>
2024-05-18 18:38:51 +0300

    +sending background email

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -65,15 +65,18 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
-
+	go func ()  {
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		if err != nil {
+			// app.serverErrorResponse(w, r, err)
+			// return
+			app.logger.PrintError(err, nil)
+		}		
+	}()
+	
 	// Write a JSON response containing the user data along with a 201 Created status
 	// code.
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
